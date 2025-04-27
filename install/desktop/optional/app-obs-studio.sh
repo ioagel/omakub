c3078ef7713daa755c60f13df2183571c49ba00c
Ioannis Angelakopoulos <ioagel@gmail.com>
2025-04-27 18:17:10 +0300

    migrates OBS installation to use the Official PPA
    - better to have the latest version

--- install/desktop/optional/app-obs-studio.sh
+++ install/desktop/optional/app-obs-studio.sh
@@ -1,2 +1,4 @@
 # OBS Studio is a screen recording application that allows you to capture both display and webcam in the same recording
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+sudo apt update
 sudo apt install -y obs-studio
