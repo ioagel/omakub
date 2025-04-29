87dbb261238832050f61a794850303cfb89e0c2a
Ioannis Angelakopoulos <ioagel@gmail.com>
2025-04-29 16:11:12 +0300

    small flatpak warning fix

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -1,9 +1,9 @@
+# To fix the XDG_DATA_DIRS warning temporarily for this session, before restarting
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share
+
 sudo apt install -y flatpak
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   sudo apt install -y gnome-software-plugin-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-# To fix the XDG_DATA_DIRS warning temporarily for this session, before restarting
-export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share
