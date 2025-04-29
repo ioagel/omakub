7e1499dcb80425406bf157856cd9b008a27f014e
Ioannis Angelakopoulos <ioagel@gmail.com>
2025-04-29 15:29:40 +0300

    Adds flatpak remote user-level
    - small flatpak warning fix

--- install/desktop/a-flatpak.sh
+++ install/desktop/a-flatpak.sh
@@ -3,3 +3,7 @@ if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   sudo apt install -y gnome-software-plugin-flatpak
 fi
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# To fix the XDG_DATA_DIRS warning temporarily for this session, before restarting
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/$USER/.local/share/flatpak/exports/share
